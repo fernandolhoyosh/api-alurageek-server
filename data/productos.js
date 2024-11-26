9beab3fdc2a61cb96a82dd8d6615fa23a5539b14
Fernando Hoyos <fernandolhoyosh@gmail.com>
2024-11-25 20:29:23 -0500

    añado 2 productos en archivo productos.js

--- data/productos.js
+++ data/productos.js
@@ -10,7 +10,19 @@ const productos = [
             nombre: "Teclado Genius USB",
             precio: "7.00",
             imagen: "https://http2.mlstatic.com/D_NQ_NP_999869-MCO79611419890_102024-O.webp"
-        }
+        },
+        {
+            id: "ae84",
+            nombre: "Cortina enrollable 110x130",
+            precio: "26.00",
+            imagen: "https://http2.mlstatic.com/D_NQ_NP_616251-MCO78192901456_082024-O.webp"
+          },
+          {
+            id: "a96b",
+            nombre: "Tijeras De Corte Recto Cortador De Láminas De L",
+            precio: "30.00",
+            imagen: "https://http2.mlstatic.com/D_NQ_NP_771025-MCO50322244088_062022-O.webp"
+          }
 ]
 
 module.exports = productos;
